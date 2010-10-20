f67f59544e9f8ba632d5906d81656ffb35fa40ba
Matias Piipari <matias.piipari@gmail.com>
2010-10-20 22:01:32 +0100

    Added the Eigen headers (version 3.0-beta2)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
 
 int main (int argc, char * const argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+	MatrixXd m(2,2);
+	m(0,0) = 3;
+	m(1,0) = 2.5;
+	m(0,1) = -1;
+	m(1,1) = m(1,0) + m(0,1);
+	std::cout << m << std::endl;
 }
