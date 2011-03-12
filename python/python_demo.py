5dd0ad59a1942b214faee361c82eb67c862ed30d
oliver.stegle <oliver.stegle@gmail.com>
2011-03-12 23:04:45 +0100

    updates

--- python/python_demo.py
+++ python/python_demo.py
@@ -1,47 +1,74 @@
-"""Demo script testing the python interface of peer"""
+"""
+PPER pythong example
+"""
 
 import sys
-#TODO: remove, this is only needed if not propperly installed:
-sys.path.append('build/lib.macosx-10.6-x86_64-2.6')
 import scipy as SP
 import peer
-import pdb
 
-#1. simulate sth.
-N = 100
+def simulate_data(N,K,sigma):
+	X = SP.random.randn(N,K)
+	W = SP.random.randn(K,G)
 
-N = 30
-K = 5
-G = 20
-sigma = 0.1
+	Y = SP.dot(X,W)
+	#simulate mean effect
+	#mean = SP.rand(N)
+	Y += Y
+	#add noise
+	Y+= sigma*SP.random.randn(N,G)
+	return Y
 
-X = SP.random.randn(N,K)
-W = SP.random.randn(K,G)
+if __name__ == '__main__':
+	#number of samples
+	N = 200
+	#number of facotrs
+	K = 5
+	#number of genes
+	G = 100
+	#noise level
+	sigma = 0.1
 
-Y = SP.dot(X,W)
-Y+= sigma*SP.random.randn(N,G)
 
-#run peer
-#use up to 10 factors
-Kinf = 20
-Nmax_iterations = 10
-vb = peer.VBFA()
-#set data and parameters
-vb.setNk(Kinf)
-vb.setAdd_mean(False)
-vb.setPhenoMean(Y)
-vb.setNmax_iterations(Nmax_iterations)
-vb.update()
+	#1. Simulate dataset
+	Y = simulate_data(N,K,sigma)
 
-#investigate inferance results
-#factors:
-X = vb.getX()
-#weights:
-W = vb.getW()
-#ARD parameters
-Alpha = vb.getAlpha()
+	#2. run PEER
+	#use up to 20 factors
+	Kinf = 20
+	#maximum number of iterations
+	Nmax_iterations = 100
 
-if 0:
-	import pylab as PL	
-	PL.figure()
-	PL.plot(Alpha)
+	vb = peer.VBFA()
+	#set data and parameters
+	#number of factor for learning
+	vb.setNk(Kinf)
+	#fit mean effect ?
+	vb.setAdd_mean(False)
+	vb.setPhenoMean(Y)
+	#set prior settings
+	#(these are the default settings of PEER)
+	vb.setPriorAlpha(0.001,0.1);
+	vb.setPriorEps(0.1,10);
+	vb.setNmax_iterations(Nmax_iterations)
+	vb.update()
+
+	#investigate inferance results
+	#factors:
+	X = vb.getX()
+	#weights:
+	W = vb.getW()
+	#ARD parameters
+	Alpha = vb.getAlpha()
+
+	#get corrected dataset:
+	Yc = vb.getResiduals()
+
+	#3. plotting (requires matplotlib)
+	if 1:
+		import pylab as PL
+		#plot relevance of factors:
+		PL.figure(1)
+		PL.plot(1/Alpha)
+		PL.xlabel('Factors')
+		PL.ylabel('Facotr Relevance')
+		
