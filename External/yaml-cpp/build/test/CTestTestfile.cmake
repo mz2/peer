e5f7c2952bc7d4c725918ebefbba7619edf7743f
Matias Piipari <matias.piipari@gmail.com>
2011-03-13 13:08:09 +0000

    Removed the accidentally added yaml-cpp/build from repo

--- External/yaml-cpp/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mz2/PearComp/peer-backup/External/yaml-cpp/test
-# Build directory: /Users/mz2/PearComp/peer-backup/External/yaml-cpp/build/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(yaml-reader-test "run-tests")
--- External/yaml-cpp/build/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mz2/PearComp/peer-backup/External/yaml-cpp/util
-# Build directory: /Users/mz2/PearComp/peer-backup/External/yaml-cpp/build/util
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
