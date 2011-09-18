8edbfd4c9a282c0fb106faedebbd79923e68e342
Matias Piipari <matias.piipari@gmail.com>
2011-09-18 19:49:13 +0100

    Create all of the src folder in create_source_package.sh. Drop src-i386 and src-x86_64 folders which get created when you R CMD INSTALL a source package from the package folder.

--- cran/create_source_package.sh
+++ cran/create_source_package.sh
@@ -2,8 +2,56 @@
 
 #shell script to update the R interface files 
 #note: this requires that a current version of PEER is build and ready to run using cmake
-rm ./peer/src/*.o
-rm ./peer/src/*.so
+rm -f ./peer/src/*.o	
+rm -f ./peer/src/*.h
+rm -f ./peer/src/*.cpp
+rm -rf ./peer/src-i386
+rm -rf ./peer/src-x86_64
+rm -f ./peer/R/peer.R
+
+ln -s $(pwd)/../External/alglib/src/dataanalysis.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/dataanalysis.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/diffequations.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/diffequations.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/fasttransforms.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/alglibinternal.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/fasttransforms.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/integration.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/integration.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/interpolation.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/interpolation.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/linalg.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/linalg.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/optimization.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/optimization.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/solvers.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/solvers.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/specialfunctions.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/specialfunctions.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/statistics.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/statistics.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/stdafx.h ./peer/src/
+
+ln -s $(pwd)/../include/ossolog.h ./peer/src/
+
+ln -s $(pwd)/../External/alglib/src/alglibmisc.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/alglibmisc.h ./peer/src/
+ln -s $(pwd)/../External/alglib/src/ap.cpp ./peer/src/
+ln -s $(pwd)/../External/alglib/src/ap.h ./peer/src/
+
+ln -s $(pwd)/../src/array_helper.cpp ./peer/src/
+ln -s $(pwd)/../include/array_helper.h ./peer/src/
+ln -s $(pwd)/../src/bayesnet.cpp ./peer/src/
+ln -s $(pwd)/../include/bayesnet.h ./peer/src/
+ln -s $(pwd)/../src/sim.cpp ./peer/src/
+ln -s $(pwd)/../include/sim.h ./peer/src/
+ln -s $(pwd)/../src/sparsefa.cpp ./peer/src/
+ln -s $(pwd)/../include/sparsefa.h ./peer/src/
+ln -s $(pwd)/../src/vbfa.cpp ./peer/src/
+ln -s $(pwd)/../include/vbfa.h ./peer/src/
+
+ln -s $(pwd)/../build/R/peerR_wrap.cxx ./peer/src/peerR_wrap.cpp
+ln -s $(pwd)/../build/R/peer.R ./peer/R/peer.R
 
 #create .tar.gz with symlinks dereferenced
 tar cfzh ./peer_source.tgz ./peer
